588996dae6167b2e0fc5323bd41d87f26c92faf8
dave-lence <davidfnwudo@gmail.com>
2023-12-12 19:01:07 +0100

    edited user network

--- mxe-app/src/screens/LoginNewUserScreen.js
+++ mxe-app/src/screens/LoginNewUserScreen.js
@@ -13,10 +13,12 @@ import ColorTheme from "../theme/colorTheme";
 import PhoneInput from "react-native-phone-number-input";
 import { Button } from "react-native-paper";
 
-const LoginNewUserScreen = ({navigation}) => {
+const LoginNewUserScreen = ({ navigation }) => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [formattedValue, setFormattedValue] = useState("");
   const [valid, setValid] = useState(false);
+  const [autoFocus, setAutoFocus] = useState(true);
+
   const phoneInput = useRef(null);
 
   const handlePhoneInputChange = (number) => {
@@ -28,10 +30,13 @@ const LoginNewUserScreen = ({navigation}) => {
   };
 
   return (
-    <KeyboardAvoidingView style={{flex:1}}  behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-    <View style={styles.container}>
-      <View style={[styles.header, { marginVertical: 10, marginTop: "15%", }]}>
-      <Button
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <View style={[styles.header, { marginVertical: 10, marginTop: "15%" }]}>
+          <Button
             style={{
               backgroundColor: ColorTheme.lightBlue,
               width: 195,
@@ -39,7 +44,7 @@ const LoginNewUserScreen = ({navigation}) => {
               left: "45%",
               padding: 0,
               borderRadius: 8,
-              marginBottom:10,
+              marginBottom: 10,
             }}
             mode="elevated"
             onPress={() => navigation.navigate("SignUpScreen")}
@@ -54,74 +59,89 @@ const LoginNewUserScreen = ({navigation}) => {
               Create account
             </Text>
           </Button>
-        <Text style={styles.welcomeText}>Welcome back!</Text>
-        <Text style={styles.descriptionText}>
+          <Text style={styles.welcomeText}>Welcome back!</Text>
+          <Text style={styles.descriptionText}>
+            Enter your phone number to link this phone to your account
+          </Text>
+        </View>
+
+        <View
+          style={{
+            backgroundColor: ColorTheme.white,
+            borderColor: autoFocus ? ColorTheme.lightBlue2 : ColorTheme.gray,
+            borderWidth: 1,
+            height: 55,
+            borderRadius: 6,
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 30,
+            padding: 5,
+          }}
+        >
+          <PhoneInput
+            ref={phoneInput}
+            defaultValue={phoneNumber}
+            defaultCode="NG"
+            layout="first"
+            value={phoneNumber}
+            onChangeText={handlePhoneInputChange}
+            onChangeFormattedText={(text) => {
+              setFormattedValue(text);
+            }}
+            countryPickerButtonStyle={{
+              marginRight: -40,
+            }}
+            withDarkTheme
+            codeTextStyle={{ height: 22 }}
+            containerStyle={{
+              width: "90",
+            }}
+            textContainerStyle={{
+              backgroundColor: "white",
+            }}
+            textInputProps={{
+              keyboardAppearance: "dark",
+              keyboardType:
+                Platform.OS === "ios" ? "name-phone-pad" : "number-pad",
+            }}
+            autoFocus={autoFocus}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 16,
+            marginTop: 18,
+            color: ColorTheme.lightGray2,
+          }}
+        >
           Enter your phone number to link this phone to your account
         </Text>
+
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            backgroundColor: !valid ? ColorTheme.darkGray : ColorTheme.darkBlue,
+            borderRadius: 10,
+            padding: 15,
+          }}
+          onPress={() => {
+            if (valid) {
+              console.log("Phone number is valid:", phoneNumber);
+              navigation.navigate("CodeVerificationScreen");
+            } else {
+              Alert.alert(
+                "Invalid Phone Number",
+                "Please enter a valid phone number."
+              );
+            }
+          }}
+        >
+          <MaterialIcons name="keyboard-arrow-right" size={24} color="white" />
+        </TouchableOpacity>
       </View>
-
-      <PhoneInput
-        ref={phoneInput}
-        defaultValue={phoneNumber}
-        defaultCode="NG"
-        layout="first"
-        value={phoneNumber}
-        onChangeText={handlePhoneInputChange}
-        onChangeFormattedText={(text) => {
-          setFormattedValue(text);
-        }}
-        withDarkTheme
-        codeTextStyle={{ height: 22 }}
-        containerStyle={{
-          borderColor: ColorTheme.darkGray,
-          width: "90",
-          borderRadius: 6,
-        }}
-        textContainerStyle={{
-          backgroundColor: ColorTheme.white,
-          borderColor: ColorTheme.darkGray,
-          borderWidth: 1,
-          height: 55,
-          borderRadius: 6,
-        }}
-        autoFocus
-      />
-
-      <Text
-        style={{
-          fontSize: 16,
-          marginTop:18,
-          color: ColorTheme.lightGray2,
-        }}
-      >
-        Enter your phone number to link this phone to your account
-      </Text>
-
-      <TouchableOpacity
-        style={{
-          position: "absolute",
-          bottom: 20,
-          right: 20,
-          backgroundColor:
-            !valid ? ColorTheme.darkGray : ColorTheme.darkBlue,
-          borderRadius: 10,
-          padding: 15,
-        }}
-        onPress={() => {
-          if (valid) {
-            console.log("Phone number is valid:", phoneNumber);
-             navigation.navigate("CodeVerificationScreen")
-          } else {
-            Alert.alert(
-              "Invalid Phone Number",
-              "Please enter a valid phone number."
-            );
-          }
-        }}
-      >
-        <MaterialIcons name="keyboard-arrow-right" size={24} color="white" />
-      </TouchableOpacity>
-    </View>
     </KeyboardAvoidingView>
   );
 };
@@ -131,7 +151,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#FFFFFF",
     paddingHorizontal: 15,
-    
   },
   header: {
     justifyContent: "start",
